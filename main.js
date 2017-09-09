5b7b2c37dfbfe340a16bfb5c2d69739018eeafc2
Lise <nakache.lise@gmail.com>
2017-09-09 20:16:34 +0300

    remove-comment-changes

--- main.js
+++ main.js
@@ -1,126 +1,142 @@
-// 1) Create a function that creates a new post object and adds it to a posts array.
-// Each post object should have two properties: text (the user's input, a string) and id 
-// (a number, dynamically generated).
-//  Each id should be unique to that post (no two post objects should have the same id).
-
-
-// 2)Now to render the array. Create a second function that adds all the posts in the posts array to the posts div.
-// In addition, add the id to the element with our data attribute. Each post element should look something like this:
-// <p class="post" data-id="1">Hey man! I'm a post!</p>
-
-// 3)
-// Change your code so that each post element also has a "remove" button. Like this:
-// <p class="post" data-id="1"> <button type="button" class="remove">REMOVE</button> Hey man! I'm a post!</p>
-// // When the button gets clicked, remove the post from the array and consequently the page.
-
-// 4)Add a feature that allows each post to receive a comment. Each post will require it's own form, allowing a
-//  user to leave their username and some kind of comment text. List all the comments and associated users below the post.
-
-
-//global variables
-var posts = [];
-var number = 0;
 // important de le mettre avant la fonction (donc dans le global scope). Si je le mets dans la fonction,
 // la variable number sera tjrs = à 0;
+// qd on utilise append, le $('element') est PARENT
+// posts.pop('p');   won't work because remove the last item of the array, and not the one we press 
+// splice : pop l'item qu'on veut !  array.splice(index, howmany, item1, ....., itemX)
+//SIBLINGS car les inputs sont dans le même div
 
-//functions
+var posts = [];
+var number = 0;
+
+
 $('.btn-primary').click(function () {
+addPost();
+
+var buttonRemovePost = '<input type="button" class="remove-post" value="Remove the post"/>';
+$('.post').append(buttonRemovePost);
+
+var buttonAddComment = '<input type="button" class="add-comment" value="Add a Comment"/>'
+$('.post').append(buttonAddComment);
+
+var buttonSubmitComment = '<input type="button" class="submit-comment" value="Submit the comment"/>'
+$('.post').append(buttonSubmitComment);
+
+var buttonRemoveComments = '<input type="button" class="remove-comment" value="Remove the comment"/>'
+$('.post').append(buttonRemoveComments);
+});
+
+
+//Click handlers
+$('.posts').on('click','.remove-post',function () {
+    removePosts(this);
+});
+
+$('.posts').on('click', '.add-comment', function () {
+    formComments(this);
+});
+
+$('.posts').on('click','.submit-comment',function () { 
+    addComment (this);
+});
+
+$('.posts').on('click','.remove-comment',function () { 
+    removeComment (this);
+});
+
+
+
+function obj (){
     var name = $('.form-control').val();
     var post = {
         text: name,
         id: number,
         comments: []
-
     };
     number++;
-    posts.push(post); // on utilise push car array !
-    addPost();
-})
+    return post;
+}
+  
+var addPost = function () {
+// add posts to the array
+var createPost = obj();
+posts.push(createPost);
 
-
-function addPost() {
-    $(".posts").find('p').remove(); // ici le off ne fonctionne pas : pk ?
+//add posts to the html
+    $(".posts").find('p').remove(); 
     for (var i = 0; i < posts.length; i++) {
-        $(".posts").append('<p>' + posts[i].text + '</p>');
-        $('p').attr('data-id', posts[i].id);
+        $(".posts").append('<p data-id=' + posts[i].id + '>' + posts[i].text + '</p>');
         $('p').addClass("post");
     }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    var buttonRemovePara = '<input type="button" class="removeparabnt" value="Remove the post"/>';
-    $('p').append(buttonRemovePara);
-    // qd on utilise append, le $('element') est PARENT
-
-    $('.removeparabnt').click(function () {
-
-        var indexPara = $(this).closest('p').index()
-        // le paragraphe proche de ce bnt= le paragraphe qu'on vient de créer
-        // [index()] donne la place de l'élément par rapport à l'objet {post}
-
-        posts.splice(indexPara, 1);
-        $(this).closest('p').remove();
-
-        // posts.pop('p');   won't work because remove the last item of the array, and not the one we press 
-        // splice : pop l'item qu'on veut !
-        //array.splice(index, howmany, item1, ....., itemX)
-    })
-
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    var buttonAddComment = '<input type="button" class="commentbtn" value="Add a Comment"/>'
-    $('.post').append(buttonAddComment);
-
-    $('.commentbtn').click(function () {
-        var formComment = '<input type="text" class="comment-name" placeholder="Post Comment"/>'
-        var usernameComment = '<input type="text" class="username-name" placeholder="Post Username"/>'
-
-        $(this).closest('.post').append('<div class="comment-form">' + '<br>' + formComment + '<br>' +
-            usernameComment + '<br>' +
-            '<input type="button" class="submitbtn" value="Submit the comment"/><div class="comments"></div></div>');
-    })
-}
-//////////////////////////////////////////////////////////////////////////////////////////////////
-
-var addComment = function (newcomment, index, post) {
-    posts[index].comments.push(newcomment);
-
-    var comments = post.find('.comments')
-
-    // mettre le paragraphe sur la page(HTML) est en 2e position
-    comments.append('<div  class="comment" >' + '<br>' + "your comment: " + newcomment.text +
-        '<br>' + " your username: " + newcomment.user + '<br>'+
-        '<input type="button" class="removecommentbtn" value="Remove the comment"/>' +
-        '</div>');
-
-
-        $('.removecommentbtn').off();
-        $('.removecommentbtn').click(function () {
-            var post = $(this).closest('.post');
-            var i = post.index();
-        
-            var comment = $(this).closest('.comment');
-            var j = comment.index();
-        
-            posts[i].comments.splice(j, 1);
-            comment.remove();
-        })
-        
-
+} 
+    
+function removePosts (a) {
+//remove posts from array
+console.log(a);
+        var post = $(a).closest('.post')
+        var indexPost = post.index();
+        posts.splice(indexPost, 1); 
+//remove posts from HTML      
+        post.remove();
 }
 
-$('.posts').on('click', ".submitbtn", function () {
-    var post = $(this).closest('.post')
-    var i = post.index();
 
-    var commentVal = $(this).siblings('.comment-name').val();
-    var usernameVal = $(this).siblings('.username-name').val();
+function formComments (b) {
+        var formComment = '<input type="text" class="comment-text" placeholder="Post Comment"/>'
+        var usernameComment = '<input type="text" class="username-text" placeholder="Post Username"/>'
+        
 
-    var commentsObj = {
-        text: commentVal,
-        user: usernameVal
+        $(b).closest('.post').append('<div class="comment-form">' + '<br>' + formComment + 
+                                        '<br>' + usernameComment + '<br>' + '<div class="comments"></div>' + '</div>');
+
+                                                            
+        $(b).hide();   
+}
+
+
+
+function addComment (c) {
+        var commentVal = $(c).siblings('.comment-form').find('.comment-text').val();
+        var usernameVal = $(c).siblings('.comment-form').find('.username-text').val();
+
+        var commentsObj = {
+            text: commentVal,
+            user: usernameVal
+        }
+
+//add comment to the array
+        var post = $(c).closest('.post')
+        var postIndex = post.index()
+
+        var commentsDiv = post.find('.comments') //descendre dans les comments donc FIND
+
+        var commenting = posts[postIndex].comments
+        commenting.push(commentsObj);
+
+//add comment to the HTML
+        commentsDiv.empty();
+        for(i=0;i<commenting.length;i++) {
+        commentsDiv.append('<div>' + '<br>' + "your comment: " + commenting[i].text + '<br>' + " your username: " + commenting[i].user + '<br>'+ '</div>');
+}
+
+
     };
-    addComment(commentsObj, i, post)
-})
+
+var removeComment = function (d) {
+//remove comment from the array   
+            // $('.removecommentbtn').off();    
+            var post = $(d).closest('.post');
+            var i = post.index();
+            var comment = post.find('.comments');
+            var j = comment.index();
+            posts[i].comments.splice(j, 1);
+
+//remove comment from HTML
+            comment.remove();
+        }
+
+
+
+
 
 
 
